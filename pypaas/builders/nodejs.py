cc943c722d73af7fbc5bb03900cc94d1a6a82f4b
Jens Gutermuth <jens.gutermuth@fintura.de>
2015-06-23 16:47:40 +0200

    refactor into function and fix second occurence as well

--- pypaas/builders/nodejs.py
+++ pypaas/builders/nodejs.py
@@ -13,19 +13,10 @@ class NPMBuilder(BaseBuilder):
     def is_applicable(self):
         return os.path.isfile(os.path.join(self.checkout.path, 'package.json'))
 
-    def build(self):
-        # Delete all the crap npm likes to leave behind
-        subprocess.check_call(
-            'rm -rf /tmp/npm-*',
-            shell=True,
-            env=self.checkout.cmd_env
-        )
-        subprocess.check_call(
-            ['npm', 'install'],
-            cwd=self.checkout.path,
-            env=self.checkout.cmd_env
-        )
-        # Delete all the crap npm likes to leave behind
+    def delete_npm_crap(self):
+        """
+        Delete all the crap npm likes to leave behind in /tmp/npm-*
+        """
         for name in os.listdir('/tmp'):
             fullname = os.path.join('/tmp', name)
             if not name.startswith('npm-') or not os.path.isdir(fullname):
@@ -34,6 +25,17 @@ class NPMBuilder(BaseBuilder):
                 shutil.rmtree(fullname)
             except:
                 pass
+
+    def build(self):
+        self.delete_npm_crap()
+
+        subprocess.check_call(
+            ['npm', 'install'],
+            cwd=self.checkout.path,
+            env=self.checkout.cmd_env
+        )
+
+        self.delete_npm_crap()
 
 
 class BowerBuilder(BaseBuilder):
