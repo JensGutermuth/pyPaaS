d490cc013c726679825ca1b1f7cc7883a3ee1875
Jens Gutermuth <jens.gutermuth@fintura.de>
2015-10-07 23:02:54 +0200

    Release 0.4.2, Bugfix SSL Domain Config

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='pyPaaS',
-    version='0.4.1',
+    version='0.4.2',
     author="Jens Gutermuth, Fintura GmbH",
     author_email="jens.gutermuth@fintura.de",
     long_description=__doc__,
