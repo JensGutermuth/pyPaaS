e01e5cca84c3eeb04d20b3a91bbb44d688418bf3
Jens Gutermuth <jens.gutermuth@fintura.de>
2015-10-19 15:18:49 +0200

    Allow multiple SSH keys per file

--- pypaas/sshkey.py
+++ pypaas/sshkey.py
@@ -15,21 +15,23 @@ class SSHKey(object):
         util.mkdir_p(os.path.join(ssh_dir, 'authorized_keys.d'))
 
         for name in os.listdir(os.path.join(ssh_dir, 'authorized_keys.d')):
-            key = open(os.path.join(ssh_dir, 'authorized_keys.d', name)).read()
-            keyparts = key.split()
-            assert keyparts[0].startswith('ssh-')
-            key = ' '.join(keyparts[:2])
-            name = name.replace('.pub', '')
-            lines.append(
-                ('command="{pypaas_cmd} $SSH_ORIGINAL_COMMAND",' +
-                 'no-agent-forwarding,no-user-rc,no-X11-forwarding,' +
-                 'no-port-forwarding {key} {name}').format(
-                    pypaas_cmd=os.path.join(
-                        os.path.dirname(sys.executable), 'pypaas'
-                    ),
-                    key=key,
-                    name=name
-                )
-            )
+            keyfilename = os.path.join(ssh_dir, 'authorized_keys.d', name)
+            with open(keyfilename) as keyfile:
+                for key in keyfile:
+                    keyparts = key.split()
+                    assert keyparts[0].startswith('ssh-')
+                    key = ' '.join(keyparts[:2])
+                    name = name.replace('.pub', '')
+                    lines.append(
+                        ('command="{pypaas_cmd} $SSH_ORIGINAL_COMMAND",' +
+                         'no-agent-forwarding,no-user-rc,no-X11-forwarding,' +
+                         'no-port-forwarding {key} {name}').format(
+                            pypaas_cmd=os.path.join(
+                                os.path.dirname(sys.executable), 'pypaas'
+                            ),
+                            key=key,
+                            name=name
+                        )
+                    )
         util.replace_file(os.path.join(ssh_dir, 'authorized_keys'),
                           '\n'.join(lines)+'\n')
